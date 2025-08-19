729c5bd67704be1fa4ee2f4f46c8e2ab5bbb6c25
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-08-19 15:27:06 +0200

    Add Transform, v3 and other helpers

--- quantum-particle-art/Adapter.cs
+++ quantum-particle-art/Adapter.cs
@@ -20,6 +20,24 @@ namespace UnityEngine
         public virtual void Awake(){}
         public virtual void Update(){}
     }
+
+    public struct Transform
+    {
+        private Godot.Transform3D _transform;
+
+        public Transform(Godot.Transform3D transform)
+        {
+            _transform = transform;
+        }
+        public static implicit operator Godot.Transform3D(Transform transform)
+        {
+            return transform._transform;
+        }
+        public static implicit operator Transform(Godot.Transform3D transform)
+        {
+            return new Transform(transform);
+        }
+    }
     public class WaitForSeconds
     {
         private float _seconds;
@@ -49,6 +67,7 @@ namespace UnityEngine
     {
         public static float Range(float min, float max) =>
             (float)GD.RandRange(min, max);
+        public static Color ColorHSV() => Color.ColorHSV(Range(0f, 1f), Range(0f, 1f), Range(0f, 1f));
     }
 
     namespace Assertions
@@ -70,13 +89,21 @@ namespace UnityEngine
     {
         public static float Pow(float f, float p) => (float)Math.Pow(f, p);
         public static float Sqrt(float f) => (float)Math.Sqrt(f);
+        public static float Abs(float f) => (float)Math.Abs(f);
+        public static int Abs(int f) => Math.Abs(f);
         public static int FloorToInt(float f) => (int)Math.Floor(f);
+        public static int RoundToInt(float f) => (int)Math.Round(f);
         public static float Atan2(float y, float x) => (float)Math.Atan2(x, y);
         public static float Cos(float f) => (float)Math.Cos(f);
         public static float Sin(float f) => (float)Math.Sin(f);
         public static float Deg2Rad => (float)(Math.PI / 180.0);
         public static float Rad2Deg => (float)(180.0 / Math.PI);
         public static float Repeat(float t, float length) => t - (float)Math.Floor(t / length) * length;
+        public static float InverseLerp(float a, float b, float value)
+        {
+            if (a == b) return 0f; // Avoid division by zero
+            return (value - a) / (b - a);
+        }
     }
 
     public struct Vector2Int
@@ -176,14 +203,67 @@ namespace UnityEngine
         public static Vector2 zero = new Vector2(0, 0);
         public static Vector2 one = new Vector2(1, 1);
     }
-
+    public struct Vector3
+    {
+        private Godot.Vector3 _vector;
+        public float x
+        {
+            get => _vector.X;
+            set => _vector.X = value;
+        }
+        public float y
+        {
+            get => _vector.Y;
+            set => _vector.Y = value;
+        }
+        public float z
+        {
+            get => _vector.Z;
+            set => _vector.Z = value;
+        }
+        public Vector3(float x, float y, float z)
+        {
+            _vector = new Godot.Vector3(x, y, z);
+        }
+    }
     public struct Color
     {
         private Godot.Color color;
-
+        public static Color black => new Color(0, 0, 0);
+        public static Color gray => new Color(0.5f, 0.5f, 0.5f);
+        public static Color white => new Color(1, 1, 1);
+        public static Color red => new Color(1, 0, 0);
+        public static Color green => new Color(0, 1, 0);
+        public static Color blue => new Color(0, 0, 1);
+        public static Color yellow => new Color(1, 1, 0);
+        public float r
+        {
+            get => color.R;
+            set => color.R = value;
+        }
+        public float g
+        {
+            get => color.G;
+            set => color.G = value;
+        }
+        public float b
+        {
+            get => color.B;
+            set => color.B = value;
+        }
+        public static Color Lerp(Color a, Color b, float t)=> new Color(a.color.Lerp(b.color, t));
         public Color(float r, float g, float b, float a = 1f)
         {
             color = new Godot.Color(r, g, b, a);
+        }
+        public Color(Godot.Color color)
+        {
+            this.color = color;
+        }
+        public static Color ColorHSV(float h, float s, float v, float a = 1f)
+        {
+            // Godot's Color constructor uses RGB, so we need to convert HSV to RGB
+            return new Color(Godot.Color.FromHsv(h, s, v, a));
         }
     }
 
