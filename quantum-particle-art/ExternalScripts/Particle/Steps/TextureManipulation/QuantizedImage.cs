171a02e9280ae6effbae29a9372e14702c71461f
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-11 15:12:46 +0200

    Moved resize to after imag been created

--- quantum-particle-art/ExternalScripts/Particle/Steps/TextureManipulation/QuantizedImage.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/TextureManipulation/QuantizedImage.cs
@@ -24,12 +24,13 @@ public class QuantizedImage : ATexProvider, IColorPicker, ISpecyPicker
 	private Godot.Image _image;
 	private Color32[] _colors;
 	private Dictionary<Color32, int> _mapBack;
+	private Vector2I _targetRes;
 
 	public QuantizedImage(Image image, int paletteSize, Vector2I targetRes)
 	{
-		image.Resize(targetRes.X, targetRes.Y, Image.Interpolation.Trilinear);
 		this._paletteSize = paletteSize;
 		this._image = image;
+		_targetRes = targetRes;
 	}
 
 
@@ -38,6 +39,7 @@ public class QuantizedImage : ATexProvider, IColorPicker, ISpecyPicker
 	{
 		while (_image == null || _image.IsEmpty())
 			await Task.Delay(100);
+		_image.Resize(_targetRes.X, _targetRes.Y, Image.Interpolation.Trilinear);
 		var format = Image.Format.Rgb8;
 		_image.Convert(format);
 		var original = _image.GetData();
