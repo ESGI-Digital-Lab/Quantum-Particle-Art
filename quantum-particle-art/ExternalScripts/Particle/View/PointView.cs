ff10cd0499624b5a5696bdcfe45603b679e799d6
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-26 18:42:05 +0200

    added a secondary lable to the point view to display the dynamic names after the first one used as main label

--- quantum-particle-art/ExternalScripts/Particle/View/PointView.cs
+++ quantum-particle-art/ExternalScripts/Particle/View/PointView.cs
@@ -11,6 +11,7 @@ public partial class PointView : Node2D, IView<Area2D, ParticleWorld>
 	[Export] private bool _showSprite = true;
 	[Export] private bool _overrideColor = false;
 	[Export] private bool _showText = true;
+	[Export] private RichTextLabel _secondaryLabel;
 	[Export] private TextureRect[] _sprites;
 	[Export] private RichTextLabel _label;
 	private Vector2 bounds;
@@ -28,17 +29,22 @@ public partial class PointView : Node2D, IView<Area2D, ParticleWorld>
 		this.GlobalPosition = ViewHelpers.Pos(info.Center / bounds, this.GetParent() as Node2D);
 		this.Scale *= new Godot.Vector2(info.Radius / bounds.x / 2f, info.Radius / bounds.y / 2f);
 		UpdateLabel(info);
+		this.Name = $"Point View {info.Gate.ShortName}";
+		_secondaryLabel.Visible = false;
 	}
 
 	private void UpdateLabel(Area2D info)
 	{
-		_label.Text = "[center]" + info.Gate.ShortName + "[/center]";
+		_label.Text = info.Gate.ShortName;
 	}
 
 	public void UpdateView(Area2D info)
 	{
 		if (info.Gate.DynamicName)
-			UpdateLabel(info);
+		{
+			_secondaryLabel.Visible = true;
+			_secondaryLabel.Text = info.Gate.ShortName;
+		}
 
 		//_root.position = ViewHelpers.WorldPosition(info.Center / bounds, _root);
 		//_scale.localScale = new Vector3(info.Radius / bounds.x, 0.1f, info.Radius / bounds.y);
