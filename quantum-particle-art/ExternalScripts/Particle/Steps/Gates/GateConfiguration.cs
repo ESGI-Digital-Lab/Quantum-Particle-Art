3486d2c7eda8ff63f6ed07d32c27c5c7ea464572
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-26 18:17:33 +0200

    Indent

--- quantum-particle-art/ExternalScripts/Particle/Steps/Gates/GateConfiguration.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Gates/GateConfiguration.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections.Generic;
 using Godot;
 using Godot.Collections;
-
-namespace DefaultNamespace.Particle.Steps.Gates;
+using UnityEngine;
+using Vector2 = Godot.Vector2;
 
 [GlobalClass]
 public partial class GateConfiguration : Godot.Resource
 {
     [Export] private AGate _gate;
-    [Export] private Godot.Collections.Array<Vector2> _positions;
+    [Export] private Godot.Collections.Array<Vector2I> _positions;
 
     public AGate Gate => _gate;
 
-    public Array<Vector2> Positions => _positions;
+    public Array<Vector2I> Positions => _positions;
 }
