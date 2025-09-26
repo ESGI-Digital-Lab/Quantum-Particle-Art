3729cf7d0dd1f742d76ffd99da8299661d2bf75a
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-26 16:54:58 +0200

    Created 2 new gates

---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Gates/Geometric/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using Godot;
+
+
+[GlobalClass]
+public partial class Rotate : AGate
+{
+    [Export] private float _degrees = 45f;
+    [Export] private bool _degInName;
+
+    public override bool Resolve(Particle particle)
+    {
+        particle.Orientation.Degrees += _degrees;
+        return true;
+    }
+
+    public override AGate Copy()
+    {
+        var ret = new Rotate();
+        ret._degrees = _degrees;
+        return ret;
+    }
+
+    public override Color Color => Colors.AliceBlue;
+
+    public override string ShortName => "Rz" + (_degInName ? Mathf.RoundToInt(_degrees).ToString() : "");
+}
---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Gates/Mathematic/Count.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+[GlobalClass]
+public partial class Count : AGate
+{
+    [Export] private string _nameAppendix;
+    [Export] private int count = 0;
+    public override bool Resolve(Particle particle)
+    {
+        count++;
+        return true;
+    }
+
+    public override AGate Copy()
+    {
+        var cnt = new Count();
+        cnt.count = count;
+        return cnt;
+    }
+
+    public override Color Color => Colors.LightGray;
+    public override string ShortName => "Cnt"+_nameAppendix;
+}
