8962bac2a66b0e6ad9de6247c87c2e5537506bcf
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-26 15:41:38 +0200

    Immplemented a ressource for gate "dictionnary"

---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Gates/GateConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Godot;
+using Godot.Collections;
+
+namespace DefaultNamespace.Particle.Steps.Gates;
+
+[GlobalClass]
+public partial class GateConfiguration : Godot.Resource
+{
+    [Export] private AGate _gate;
+    [Export] private Godot.Collections.Array<Vector2> _positions;
+
+    public AGate Gate => _gate;
+
+    public Array<Vector2> Positions => _positions;
+}
