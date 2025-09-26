1d69bcf2c5e224c7f1fb70985f657edb8c936158
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-26 18:17:16 +0200

    Made count optionnaly show it's count instead of name

--- quantum-particle-art/ExternalScripts/Particle/Steps/Gates/Mathematic/Count.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Gates/Mathematic/Count.cs
@@ -3,21 +3,25 @@
 [GlobalClass]
 public partial class Count : AGate
 {
+    [Export] private bool _countAsName = true;
     [Export] private string _nameAppendix;
-    [Export] private int count = 0;
+    [Export] private int _count = 0;
+
     public override bool Resolve(Particle particle)
     {
-        count++;
+        _count++;
         return true;
     }
 
     public override AGate Copy()
     {
         var cnt = new Count();
-        cnt.count = count;
+        cnt._count = _count;
         return cnt;
     }
 
     public override Color Color => Colors.LightGray;
-    public override string ShortName => "Cnt"+_nameAppendix;
+    public override string ShortName => (_countAsName ? _count.ToString() : "Cnt") + _nameAppendix;
+
+    public override bool DynamicName => base.DynamicName || _countAsName;
 }
