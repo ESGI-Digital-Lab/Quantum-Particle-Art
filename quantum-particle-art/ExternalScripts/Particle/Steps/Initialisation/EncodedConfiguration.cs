e30e80d90a3208c98b38f5f83230ef7a997e7b0c
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-28 15:30:56 +0200

    Created encoded configuration, holding on a byte on n bits represented by right going particles

---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/EncodedConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+[GlobalClass]
+public partial class EncodedConfiguration : ASpawnConfiguration
+{
+    [Export] private int _encoded;
+
+    public override IEnumerable<UnityEngine.Vector2> Particles(Random random)
+    {
+        var _encoded = Mathf.Clamp(this._encoded, 0, (int)Mathf.Pow(2, _nbParticles) - 1);
+        bool[] bits = new bool[_nbParticles];
+        for (int i = 0; i < _nbParticles; i++)
+        {
+            bits[i] = (_encoded & 1) == 1; //Check last bit is 1
+            _encoded >>= 1; //Bit shift
+        }
+
+        int cnt = 0;
+        foreach (var p in LinearReparition(new(0, 0), new(0, 1), _nbParticles))
+        {
+            if (bits[cnt])
+                yield return p;
+            cnt++;
+        }
+    }
+
+    protected override UnityEngine.Vector2 BaseVelocity()
+    {
+        return new(1, 0f);
+    }
+}
