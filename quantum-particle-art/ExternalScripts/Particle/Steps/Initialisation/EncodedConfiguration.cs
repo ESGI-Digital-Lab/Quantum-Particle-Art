edf522486e1d9024ea917db1fd083c9350ac29a4
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-28 16:12:46 +0200

    Encoded configuration now provided a dynamic grid gates based on encoding parameters

--- quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/EncodedConfiguration.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/EncodedConfiguration.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
+using Godot.Collections;
 
 [GlobalClass]
 public partial class EncodedConfiguration : ASpawnConfiguration
 {
     [Export] private int _encoded;
+    private IGates _gates;
+    public override IGates Gates => _gates ??= GenerateGates();
+
+    private IGates GenerateGates()
+    {
+        GridGates gates = new GridGates(new(_nbParticles, _nbParticles), new(_nbParticles - 1, 0), [
+            new(new Kill(), Enumerable.Range(0, _nbParticles).Select(i => new Vector2I(0, i))),
+            new(new Count(), Enumerable.Range(0, _nbParticles).Select(i => new Vector2I(-1, i)))
+        ]);
+        return gates;
+    }
 
     public override IEnumerable<UnityEngine.Vector2> Particles(Random random)
     {
