f6e76babba7a6827595f4ef96210cdb3535d1e5d
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-28 16:11:35 +0200

    Indent

--- quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/SpawnConfiguration.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/SpawnConfiguration.cs
@@ -1,47 +1,50 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Godot;
 
 [GlobalClass]
 public partial class SpawnConfiguration : ASpawnConfiguration
 {
-    [Export(PropertyHint.Link)] private Vector2 _baseVelocity;
-    [ExportGroup("Positions")] [Export] private Vector2 _center;
-    [Export(PropertyHint.Link)] private Vector2 _size;
+	[Export(PropertyHint.Link)] private Vector2 _baseVelocity;
+	[ExportGroup("Positions")] [Export] private Vector2 _center;
+	[Export(PropertyHint.Link)] private Vector2 _size;
+	[Export] private bool _linSpaceOverRandom = false;
+	[ExportGroup("Gates")]
+	[Export] private GridGates _gates;
+	public override GridGates Gates => _gates;
 
-    private Vector2 _posMin => _center - (_size / 2f);
-    private Vector2 _posMax => _center + (_size / 2f);
-    [Export] private bool _linSpaceOverRandom = false;
-    private Random _random;
+	private Vector2 _posMin => _center - (_size / 2f);
+	private Vector2 _posMax => _center + (_size / 2f);
+	private Random _random;
 
-    private float RandomRange(Vector2 range)
-    {
-        return RandomRange(range.X, range.Y);
-    }
+	private float RandomRange(Vector2 range)
+	{
+		return RandomRange(range.X, range.Y);
+	}
 
-    private float RandomRange(float min, float max)
-    {
-        return ((float)_random.NextDouble() * (max - min)) + min;
-    }
+	private float RandomRange(float min, float max)
+	{
+		return ((float)_random.NextDouble() * (max - min)) + min;
+	}
 
-    public override IEnumerable<UnityEngine.Vector2> Particles(System.Random random)
-    {
-        this._random = random;
-        var lb = _posMin;
-        var ub = _posMax;
-        if (_linSpaceOverRandom)
-        {
-            foreach (var vector2 in LinearReparition(lb, ub, _nbParticles)) yield return vector2;
-        }
-        else
-        {
-            for (int i = 0; i < _nbParticles; i++)
-            {
-                Vector2 normalizedPos = new Vector2(RandomRange(lb.X, ub.X), RandomRange(lb.Y, ub.Y));
-                yield return normalizedPos;
-            }
-        }
-    }
+	public override IEnumerable<UnityEngine.Vector2> Particles(System.Random random)
+	{
+		this._random = random;
+		var lb = _posMin;
+		var ub = _posMax;
+		if (_linSpaceOverRandom)
+		{
+			foreach (var vector2 in LinearReparition(lb, ub, _nbParticles)) yield return vector2;
+		}
+		else
+		{
+			for (int i = 0; i < _nbParticles; i++)
+			{
+				Vector2 normalizedPos = new Vector2(RandomRange(lb.X, ub.X), RandomRange(lb.Y, ub.Y));
+				yield return normalizedPos;
+			}
+		}
+	}
 
-    protected override UnityEngine.Vector2 BaseVelocity() => _baseVelocity;
+	protected override UnityEngine.Vector2 BaseVelocity() => _baseVelocity;
 }
