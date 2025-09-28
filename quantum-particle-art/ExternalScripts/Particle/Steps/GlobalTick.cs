b9ebad9bd40e9ff65754f0fb8b840c5be72c8da4
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-28 16:55:26 +0200

    Added an event triggered once when all particles died

--- quantum-particle-art/ExternalScripts/Particle/Steps/GlobalTick.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/GlobalTick.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class GlobalTick : ParticleStep
 {
     private float _timeSteps;
+    private bool _autoStop;
     private IColorPicker _colorPicker;
-    public GlobalTick(float timeSteps){
+    public event Action onAllDead;
+    public GlobalTick(float timeSteps, bool stopOnAllDead = true){
         _timeSteps = timeSteps;
+        _autoStop = stopOnAllDead;
     }
     public record struct MovementData(Vector2 fromNormalized, Vector2 toNormalize, Color color, Orientation orientation);
 
@@ -23,11 +27,13 @@ public class GlobalTick : ParticleStep
     {
         float t = _timeSteps; //_useDeltaTime ? Time.deltaTime : _timeSteps;
 
+        bool moved = false;
         for (var index = 0; index < entry.Count; index++)
         {
             var particle = entry[index];
             foreach (var info in particle.Tick(t, entry.Ruleset[particle.Species].Friction))
             {
+                moved = true;
                 var data = new MovementData(info.fromNormalized, info.particle.NormalizedPosition,
                     _colorPicker.GetColor(info.particle, entry.Ruleset.NbSpecies)/(info.depth+1),
                     info.particle.Orientation);
@@ -36,6 +42,12 @@ public class GlobalTick : ParticleStep
 
             if (delay > 0)
                 await Task.Delay((int)(delay*1000));
+        }
+
+        if (!moved)
+        {
+            onAllDead?.Invoke();
+            onAllDead = null;
         }
     }
 
