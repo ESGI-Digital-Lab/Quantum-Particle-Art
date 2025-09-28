178bb497596a7f653ac894b4630cb26477e3ae7a
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-09-28 16:56:42 +0200

    Reversed binary order and added a result calculation, using "binary" values of the count gates
    Values coould be more than 1 actually if there wasn't a kill gate behind or if particle came from a different angle, later we might created a composed gate that trigger the two effects

--- quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/EncodedConfiguration.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/EncodedConfiguration.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 using Godot.Collections;
+using UnityEngine.Assertions;
 
 [GlobalClass]
 public partial class EncodedConfiguration : ASpawnConfiguration
 {
+    [Export] private Count _countTemplate;
+    [Export] private Kill _killTemplate;
     [Export] private int _encoded;
-    private IGates _gates;
+    private GridGates _gates;
     public override IGates Gates => _gates ??= GenerateGates();
 
-    private IGates GenerateGates()
+    private GridGates GenerateGates()
     {
         GridGates gates = new GridGates(new(_nbParticles, _nbParticles), new(_nbParticles - 1, 0), [
-            new(new Kill(), Enumerable.Range(0, _nbParticles).Select(i => new Vector2I(0, i))),
-            new(new Count(), Enumerable.Range(0, _nbParticles).Select(i => new Vector2I(-1, i)))
+            new(_killTemplate, Enumerable.Range(0, _nbParticles).Select(i => new Vector2I(0, i))),
+            new(_countTemplate, Enumerable.Range(0, _nbParticles).Select(i => new Vector2I(-1, i))),
+            //new(new Rotate(45), [new(-3,0)])
         ]);
         return gates;
+    }
+
+    public int Result()
+    {
+        var copies = _gates.Copies(_countTemplate).ToArray();
+        Assert.IsTrue(copies.Length == _nbParticles, $"Count gate copies {copies.Length} does not match number of particles {_nbParticles}");
+        int acc = 0;
+        int i = 0;
+        foreach (var copy in copies)
+        {
+            acc += copy.Value * (int) Mathf.Pow(2, i);
+            i++;
+        }
+        return acc;
     }
 
     public override IEnumerable<UnityEngine.Vector2> Particles(Random random)
@@ -26,7 +44,7 @@ public partial class EncodedConfiguration : ASpawnConfiguration
         bool[] bits = new bool[_nbParticles];
         for (int i = 0; i < _nbParticles; i++)
         {
-            bits[i] = (_encoded & 1) == 1; //Check last bit is 1
+            bits[_nbParticles-(i+1)] = (_encoded & 1) == 1; //Check last bit is 1
             _encoded >>= 1; //Bit shift
         }
 
