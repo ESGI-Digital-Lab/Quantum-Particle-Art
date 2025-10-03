3faa6402f162a97a4e56d967a619c68a58ee8853
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-03 13:00:51 +0200

    Negative values can be passed with "inverted" Conditionned color variant base on "inverted"

--- quantum-particle-art/ExternalScripts/Particle/Steps/Gates/Geometric/Rotate.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Gates/Geometric/Rotate.cs
@@ -4,27 +4,29 @@
 [GlobalClass]
 public partial class Rotate : AGate
 {
-    [Export] private float _degrees;
+    [Export(PropertyHint.Range,"0,360,1")] private float _degrees;
+    [Export] private bool _inverted = false;
 
     public Rotate() : this(45f)
     {
     }
 
-    public Rotate(float degrees)
+    public Rotate(float degrees, bool inverted = false)
     {
+        _inverted = inverted;
         _degrees = degrees;
     }
 
     public override bool Resolve(Particle particle)
     {
-        particle.Orientation.Degrees += _degrees;
+        particle.Orientation.Degrees +=  _inverted ? -_degrees : _degrees;
         return true;
     }
 
-    public override string Label => base.Label + (_degrees > 0 ? "+" : "") + _degrees.ToString("0.#");
+    
+    public override string Label => base.Label + (_inverted ? "-" : "+") + _degrees.ToString("0.#");
 
-
-    public override Color Color => Colors.SteelBlue;
+    public override Color Color => _inverted ? Colors.MediumSlateBlue : Colors.DarkBlue;
 
     public override string ShortName => "Rz";
 }
