f322df7c95d1a69c99ea2d4a9c8c378e0d0b26e2
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-05 13:12:46 +0200

    Created averaged fitness, repeating a same fitness multiple time (considering it will change it's condition on each run)

--- quantum-particle-art/ExternalScripts/Particle/Genetics/Fitnesses.cs
+++ quantum-particle-art/ExternalScripts/Particle/Genetics/Fitnesses.cs
@@ -1,37 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using GeneticSharp;
 using UnityEngine.ExternalScripts.Particle.Genetics;
 
+public class AveragedFitness : IFitness
+{
+    private IFitness _fitness;
+    private int _numberEvaluations;
+
+    public AveragedFitness(IFitness fitness, int numberEvaluations)
+    {
+        _fitness = fitness;
+        _numberEvaluations = numberEvaluations;
+    }
+
+    public int NumberEvaluations
+    {
+        get => _numberEvaluations;
+        set => _numberEvaluations = value;
+    }
+
+    public double Evaluate(IChromosome chromosome)
+    {
+        double acc = 0;
+        object accLock = new();
+        //Parallel.ForAsync(0, _numberEvaluations, (i,c)  =>
+        //{
+        //    return ValueTask.CompletedTask;
+        //}).Wait();
+        Task[] tasks = new Task[_numberEvaluations];
+        for (int i = 0; i < _numberEvaluations; i++)
+        {
+            tasks[i] = Task.Run(() =>
+            {
+                var val = _fitness.Evaluate(chromosome);
+                lock (accLock)
+                    acc += val;
+            });
+        }
+
+        Task.WaitAll(tasks);
+        return acc / _numberEvaluations;
+    }
+}
+
 public class CombinedFitness : IFitness
 {
-    private (IFitness, float)[] _fitnesses;
+    private (IFitness, double)[] _fitnesses;
 
-    public CombinedFitness(params (IFitness, float)[] fitnesses)
+    public CombinedFitness(params (IFitness, double)[] fitnesses)
     {
         _fitnesses = fitnesses.ToArray();
     }
 
     public double Evaluate(IChromosome chromosome)
     {
-        double sum = 0;
-        float totalWeight = 0;
-        foreach (var (fitness, weight) in _fitnesses)
-        {
-            sum += fitness.Evaluate(chromosome) * weight;
-            totalWeight += weight;
-        }
-
-        //Debug.LogError("Use then logic, using only second fitness only if first is full");
-
-        if (totalWeight > 0)
-        {
-            return sum / totalWeight;
-        }
-        else
-        {
-            return 0;
-        }
+        var vws = _fitnesses.Select(fw => (fw.Item1.Evaluate(chromosome), fw.Item2));
+        return BitHelpers.WeightedSum(vws);
     }
 }
 
@@ -44,7 +71,7 @@ public class MostNullGates : IFitness
         foreach (var ch in genes)
         {
             var gene = (GeneContent)ch.Value;
-            if (GatesTypesToInt.IsNullId(gene.TypeId)) cnt--;
+            if (!GatesTypesToInt.IsNullId(gene.TypeId)) cnt--;
         }
 
         return cnt / genes.Length;
