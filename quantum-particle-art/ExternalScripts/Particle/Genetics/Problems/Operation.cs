d6e52437defc0f771fbd5f0d2948f0ca07e31621
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-05 17:56:09 +0200

    Playing with another operation

--- quantum-particle-art/ExternalScripts/Particle/Genetics/Problems/Operation.cs
+++ quantum-particle-art/ExternalScripts/Particle/Genetics/Problems/Operation.cs
@@ -9,5 +9,5 @@
 
     public int CreateNewInput() => UnityEngine.Random.Range(1, (_maxValue + 1) / 2);
 
-    public int Expected(int input) => input * 2;
+    public int Expected(int input) => input * 2 - 1;
 }
