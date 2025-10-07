72f8a2c6bc637f295b469af408728300a04f9faa
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-07 18:32:09 +0200

    Created a new intermediary interface to determine width of line from particle info and separated impl and itnerface in dedicated files

---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/TextureManipulation/IWidther.cs
@@ -0,0 +1,10 @@
+﻿using Godot;
+
+public interface IWidther
+{
+    public float DetermineWidth(Particle data)
+    {
+        return DetermineWidth(data.Orientation.Velocity);
+    }
+    public float DetermineWidth(Vector2 velocity);
+}
---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/TextureManipulation/ToggleLiner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DefaultNamespace.Tools;
+using Godot;
+
+public class ToggleLiner : ILiner, IWidther
+{
+    private float _sineAmplitude = -1f;
+    private bool _speedOverSine => _sineAmplitude < 0;
+    private bool _dynamicMax;
+
+    public ToggleLiner(bool dynamicMax)
+    {
+        _dynamicMax = dynamicMax;
+        if (_dynamicMax)
+            Orientation.MaxSpeed = .1f;
+    }
+
+    public ToggleLiner(float sineAmplitude)
+    {
+        _sineAmplitude = sineAmplitude;
+    }
+
+    public LineCollection.Line CreateLine(GlobalTick.MovementData data)
+    {
+        var width = (this as IWidther).DetermineWidth(data.orientation.Owner);
+
+        var line = ILiner.Line(data, width);
+        return line;
+    }
+
+    public float DetermineWidth(Vector2 data)
+    {
+        var width = 0f;
+        if (_speedOverSine)
+        {
+            float speed = data.Length();
+            if (_dynamicMax && speed > Orientation.MaxSpeed)
+                Orientation.MaxSpeed = speed;
+            width = speed / Orientation.MaxSpeed;
+        }
+        else
+        {
+            width = (MathF.Sin(Godot.Time.GetTicksMsec() * 1000f * _sineAmplitude) + 1f) / 2f;
+        }
+
+        return width;
+    }
+}
