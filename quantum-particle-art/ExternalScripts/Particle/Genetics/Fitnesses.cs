44090e293c93d90d3390c43d6bd8d150e2349648
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-09 10:42:24 +0200

    Moved aveeragedfitness to dedicated file

--- quantum-particle-art/ExternalScripts/Particle/Genetics/Fitnesses.cs
+++ quantum-particle-art/ExternalScripts/Particle/Genetics/Fitnesses.cs
@@ -5,36 +5,6 @@ using System.Threading.Tasks;
 using GeneticSharp;
 using UnityEngine.ExternalScripts.Particle.Genetics;
 
-public class AveragedFitness : IFitness
-{
-    private ParticleSimulatorFitness _fitness;
-    private int _numberEvaluations;
-
-    public AveragedFitness(ParticleSimulatorFitness fitness, int numberEvaluations)
-    {
-        _fitness = fitness;
-        _numberEvaluations = numberEvaluations;
-    }
-
-    public int NumberEvaluations
-    {
-        get => _numberEvaluations;
-        set => _numberEvaluations = value;
-    }
-
-    public double Evaluate(IChromosome chromosome)
-    {
-        double acc = 0;
-        var task = _fitness.Evaluate(chromosome, _numberEvaluations);
-        task.Wait();
-        var val = task.Result;
-        for (int i = 0; i < _numberEvaluations; i++)
-            acc += val[i];
-
-        return acc / _numberEvaluations;
-    }
-}
-
 public class CombinedFitness : IFitness
 {
     private (IFitness, double)[] _fitnesses;
