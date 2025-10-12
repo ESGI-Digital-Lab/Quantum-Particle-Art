116991fbd68cc5d27c57f617a875413a43345b46
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-12 18:28:16 +0200

    Moved file

---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/GateConfigurations/ChromosomeConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using GeneticSharp;
+using Godot;
+using UnityEngine.ExternalScripts.Particle.Genetics;
+
+[GlobalClass]
+public partial class ChromosomeConfiguration : Resource
+{
+    [Export] private Godot.Collections.Array<GateConfiguration> _gates;
+    [Export] protected Vector2I _size;
+
+    private int? _max = null;
+    public virtual IEnumerable<GateConfiguration> GatesConfig => _gates;
+
+    public int RandomInput
+    {
+        get
+        {
+            //We are in a ressource, created and used in different runs, where each non exported variable is flushed on game stop
+            _max ??= (int)Mathf.Pow(2, _size.Y) - 1;
+            return UnityEngine.Random.Range(1, _max.Value + 1);
+        }
+    }
+
+    public ChromosomeConfiguration() : this([])
+    {
+    }
+
+    public ChromosomeConfiguration(Vector2I size) : this([], size)
+    {
+    }
+
+    public ChromosomeConfiguration(IEnumerable<GateConfiguration> gateChromosome) : this(gateChromosome, new Vector2I(10, 10))
+    {
+    }
+
+    public ChromosomeConfiguration(IEnumerable<GateConfiguration> gateChromosome, Vector2I size)
+    {
+        _gates = new(gateChromosome);
+        _size = size;
+    }
+
+    public ChromosomeConfiguration(GateChromosome gateChromosome, Vector2I size) : this(
+        BitHelpers.GetGates(gateChromosome, size), size)
+    {
+    }
+}
