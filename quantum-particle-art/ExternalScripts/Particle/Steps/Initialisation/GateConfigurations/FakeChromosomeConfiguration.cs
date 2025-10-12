b64d4b01aa61684a4a74fa73a6e643ce8a6bdcf3
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-10-12 18:28:45 +0200

    Created an extending type of configuration that interpret the list as a sample line and copies it on all line

---
+++ quantum-particle-art/ExternalScripts/Particle/Steps/Initialisation/GateConfigurations/FakeChromosomeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+[GlobalClass]
+public partial class FakeChromosomeConfiguration : ChromosomeConfiguration
+{
+    public override IEnumerable<GateConfiguration> GatesConfig
+    {
+        get
+        {
+            return base.GatesConfig.SelectMany(g =>
+            {
+                return Enumerable.Range(0, _size.Y).Select(i =>
+                {
+                    return new GateConfiguration(g.Gate, g.Positions.Select(p => new Vector2I(p.X, i)));
+                });
+            });
+        }
+    }
+}
