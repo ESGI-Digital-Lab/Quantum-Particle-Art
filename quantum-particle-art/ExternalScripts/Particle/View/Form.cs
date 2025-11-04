615d1e41fbbce4ae718cd57c1df5164a18a244b7
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-11-04 18:36:33 +0100

    Bound submit button + signal from field to another event sending the mail

--- quantum-particle-art/ExternalScripts/Particle/View/Form.cs
+++ quantum-particle-art/ExternalScripts/Particle/View/Form.cs
@@ -1,11 +1,24 @@
 ﻿using Godot;
 
 
-public partial class Form : Node
+public partial class Form : Control
 {
     [Export] private Control _root;
-    [Export] private Godot.TextEdit _inputField;
-    public string Mail   
+    [Export] private Godot.LineEdit _inputField;
+    [Export] private Button _submit;
+    public event System.Action<string> OnSubmit;
+    public override void _Ready()
+    {
+        _submit.Pressed += () =>
+        {
+            OnSubmit?.Invoke(Mail);
+        };
+        _inputField.TextSubmitted += (string text) =>
+        {
+            OnSubmit?.Invoke(Mail);
+        };
+    }
+    private string Mail   
     {
         get
         {
