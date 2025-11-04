8e2867ed8058ecd753657cce3950446824e972dc
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-11-04 17:24:39 +0100

    Created and tested a Send method taking an image, a name and reading the credentials from secret file

--- quantum-particle-art/ExternalScripts/Particle/Steps/SendImage.cs
+++ quantum-particle-art/ExternalScripts/Particle/Steps/SendImage.cs
@@ -1,16 +1,55 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Godot;
+using MailKit.Net.Smtp;
+using MimeKit;
 using UnityEngine.Assertions;
 
 namespace UnityEngine.ExternalScripts.Particle.Steps;
 
 public class SendImage : ParticleStep
 {
+    [Export] private Form _form;
     private Saver _saver;
     private bool _sent = false;
+
+    private Dictionary<string, string> secrets;
+    string secretFileName = "secrets.json";
+
+    enum Keys
+    {
+        Host = 0,
+        User = 1,
+        Password = 2,
+        DefaultTo = 3
+    }
+
+    string[] _keys = { "Host", "User", "Password", "DefaultTo" };
+    private string Secret(Keys key) => secrets[_keys[(int)key]];
 
     public SendImage(Saver saver)
     {
         _saver = saver;
+        string file;
+        try
+        {
+            file = File.ReadAllText(secretFileName);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(
+                $"Error while reading secret file {secretFileName} under {Directory.GetCurrentDirectory()}, make sure it's been created properly on this machine. Exception: {e}");
+            return;
+        }
+
+        secrets = JsonSerializer.Deserialize<Dictionary<string, string>>(file);
+        Assert.IsTrue(_keys.All(k => secrets.ContainsKey(k) && !string.IsNullOrEmpty(secrets[k])),
+            () => $"Make sure your {secretFileName} file contains all required keys: {string.Join(", ", _keys)}");
+        Send(Secret(Keys.DefaultTo), File.OpenRead(@"D:/Downloads/Black2048_times_2_final_8.png"), "Test mail with attachment");
     }
 
     public override async Task Init(WorldInitializer initializer)
@@ -31,7 +70,46 @@ public class SendImage : ParticleStep
         {
             Assert.IsTrue(_saver.Saved != null && _saver.Saved.Exists, "_saver.Saved!=null && _saver.Saved.Exists");
             Debug.Log($"Sending image {_saver.Saved.Name} in {_saver.Saved.Directory}");
-            _sent = true;
+            var mail = _form.Mail;
+            if (string.IsNullOrEmpty(mail))
+                mail = Secret(Keys.DefaultTo);
+            if (Send(mail, File.OpenRead(_saver.Saved.FullName), _saver.Name))
+                _sent = true;
         }
+    }
+
+    public bool Send(string to, FileStream attachement, string name)
+    {
+        var server = Secret(Keys.Host);
+        var user = Secret(Keys.User);
+        var password = Secret(Keys.Password);
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Florentin Eraud", user));
+        message.To.Add(new MailboxAddress("Florentin Eraud R", to));
+        message.Subject = "Test mail'?";
+
+        var fullBody = new Multipart();
+        fullBody.Add(new TextPart("plain")
+        {
+            Text = "Here is the image you wanted."
+        });
+        fullBody.Add(new MimePart("image", "png")
+        {
+            Content = new MimeContent(attachement),
+            ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+            ContentTransferEncoding = ContentEncoding.Base64,
+            FileName = name
+        });
+        message.Body = fullBody;
+        using (var client = new SmtpClient())
+        {
+            client.CheckCertificateRevocation = false;
+            client.Connect(server, 465, true);
+            client.Authenticate(user, password);
+            client.Send(message);
+            client.Disconnect(true);
+        }
+
+        return true;
     }
 }
---
+++ quantum-particle-art/ExternalScripts/Particle/View/Form.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+
+public partial class Form : Node
+{
+    [Export] private Control _root;
+    [Export] private Godot.TextEdit _inputField;
+    public string Mail   
+    {
+        get
+        {
+            var mail = _inputField.Text.Trim();
+            return mail;
+        }
+    }
+    public bool Visible   
+    {
+        set => _root.Visible = value;
+    }
+}
