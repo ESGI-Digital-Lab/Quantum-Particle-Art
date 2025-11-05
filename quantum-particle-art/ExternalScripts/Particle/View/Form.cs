e84037c50eb59f6c73c2b00690ecaa9b2c84c5ac
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-11-05 11:52:24 +0100

    Added option to externally highligth text in given color until reedited (on error on submit)

--- quantum-particle-art/ExternalScripts/Particle/View/Form.cs
+++ quantum-particle-art/ExternalScripts/Particle/View/Form.cs
@@ -4,21 +4,29 @@ using KGySoft.CoreLibraries;
 
 public partial class Form : Control
 {
+    [ExportGroup("References")]
     [Export] private Control _root;
     [Export] private Godot.LineEdit _inputField;
     [Export] private Button _submit;
     [Export] private Button _close;
+    [ExportGroup("Settings")]
+    [Export] private bool _visibleOnStart = false;
+    [Export] private Color _error;
+    [Export] private string _textColorKey = "font_color";
     public event System.Action<string> OnSubmit;
     public event System.Action OnExit;
-
     public override void _Ready()
     {
         base._EnterTree();
         _submit.Pressed += () => { OnSubmit?.Invoke(Mail); };
         _inputField.TextSubmitted += (string text) => { OnSubmit?.Invoke(Mail); };
+        _inputField.EditingToggled += on =>
+        {
+            if(on)
+                _inputField.RemoveThemeColorOverride(_textColorKey);
+        };
         _close.Pressed += () => { OnExit?.Invoke(); };
         OnExit += Exited;
-
     }
     
 
@@ -46,8 +54,18 @@ public partial class Form : Control
         }
     }
 
+    public void Recolor()
+    {
+        _inputField.AddThemeColorOverride(_textColorKey, _error);
+    }
+
     private bool Entered()
     {
         return _root.Visible = true;
+    }
+
+    public void SetInitState()
+    {
+        this.Visible = _visibleOnStart;
     }
 }
